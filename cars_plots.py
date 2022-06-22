93e60cd9ce3ff9924dd365e3cd69c13eee6de0ca
Samuel  Batista Martins <s.martins@kigroup.de>
2022-06-22 17:11:30 +0100

    multiple graphs

--- cars_plots.py
+++ cars_plots.py
@@ -86,24 +86,52 @@ x = sorted_by_year.keys()
 # corresponding y axis values
 y = []
 
+y_weight = []
+y_cylinders = []
+
 for key in sorted_by_year.keys():
     average_horsepower = 0
+    average_weight_lbs = 0
+    average_cylinders = 0
     for car in sorted_by_year[key]:
         average_horsepower += car.horsepower
+        average_weight_lbs += car.weight_lbs
+        average_cylinders += car.cylinders
     cnt = len(sorted_by_year[key])
     average_horsepower /= cnt
+    average_cylinders /= cnt
+    average_weight_tons = (0.45359237 * average_weight_lbs/cnt)/1000
     y.append(average_horsepower)
+    y_weight.append(average_weight_tons*170)
+    y_cylinders.append(average_cylinders * 50)
 
-# plotting the points
-plt.plot(x, y)
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
-# naming the x axis
-plt.xlabel('Year')
-# naming the y axis
-plt.ylabel('HorsePower')
+ax1.plot(x, y, label="Avg. Horse Power")
+ax1.plot(x, y_weight, label="Avg. Weight")
+ax1.plot(x, y_cylinders, label="Avg. Cylinders")
+ax1.set_title(
+    "Blue: Horsepower\nOrange: Weight\nGreen: Cylinders")
 
-# giving a title to my graph
-plt.title('Average Horsepower over the years...')
+x = sorted(sorted_by_cylinders.keys())
+y = []
+
+cyl_mpg_keys = sorted(sorted_by_cylinders)
+for key in cyl_mpg_keys:
+    average_mpg = 0
+    cnt = 0  # incremented manually, some cars do not have mpg data
+    for car in sorted_by_cylinders[key]:
+        if car.miles_per_gallon is None:
+            continue
+        average_mpg += car.miles_per_gallon
+        cnt += 1
+    # convert mpg to l/100km
+    average_liter_per_100km = 235.215/(average_mpg/cnt)
+    y.append(average_liter_per_100km)
+
+ax2.plot(x, y, 'tab:red')
+ax2.set_title("Cylinders vs L/100 Km")
 
 # function to show the plot
+plt.legend()
 plt.show()
